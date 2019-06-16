648151dc90ca467dcc4748f77fe449dd0c7126b9
Paloma <palolili@gmail.com>
2019-06-16 21:47:18 +0200

    Updated the function to extract users and use timeline on the users vector

--- 02_R/data_loading.R
+++ 02_R/data_loading.R
@@ -23,3 +23,30 @@ tweets_biocond <- get_timelines("bioconductor",
 # Timeline
 
 ts_plot(tweets, "month")
+
+# Extracting a sample of the names from the previous week
+
+#Pulls a list of user names
+
+data_users <- data_week %>% 
+  count(screen_name) %>% 
+  arrange(desc(n)) %>% 
+  pull(screen_name)
+
+length(data_users)
+
+#Samples a number of users from the previous vector
+data_users_sample <- sample(data_users, 20)
+
+data_users_sample
+
+# Extracts the timeline of the previous users
+test <- get_timelines(data_users_sample, 
+                      n = 1000, 
+                      language = 'en',
+                      since = '2015-01-01', 
+                      until = '2019-06-15',
+                      token = token)
+
+library(rio)
+export(test, "./03_rawdata/test_20users.RData")
